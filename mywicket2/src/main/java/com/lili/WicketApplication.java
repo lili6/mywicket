bb5f467f8d1630b470effd87616ad63df5ca11a1
lili6 <34793278@qq.com>
2015-04-11 02:13:15 +0800

    test BookMarkableLink convert to Link OK

---
+++ mywicket2/src/main/java/com/lili/WicketApplication.java
@@ -0,0 +1,39 @@
+package com.lili;
+
+import org.apache.wicket.markup.ComponentTag;
+import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.link.BookmarkablePageLink;
+import org.apache.wicket.protocol.http.WebApplication;
+
+/**
+ * Application object for your web application.
+ * If you want to run this application without deploying, run the Start class.
+ * 
+ * @see com.lili.Start#main(String[])
+ */
+public class WicketApplication extends WebApplication
+{
+	/**
+	 * @see org.apache.wicket.Application#getHomePage()
+	 */
+	@Override
+	public Class<? extends WebPage> getHomePage()
+	{
+
+		return HomePage.class;
+	}
+
+	/**
+	 * @see org.apache.wicket.Application#init()
+	 */
+	@Override
+	public void init()
+	{
+		super.init();
+//		getFrameworkSettings().setSerializer();
+
+
+
+		// add your configuration here
+	}
+}
